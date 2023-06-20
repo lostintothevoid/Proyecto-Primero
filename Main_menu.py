5940dd6a0835f9ea9ced95dbb9ebc5f583818ef3
lostintothevoid <alvaro.clancata@gmail.com>
2023-06-19 20:03:34 -0700

    avance en menus de interfaz grafica 19/06

--- Main_menu.py
+++ Main_menu.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-import tkinter.messagebox
 import customtkinter as ctk
-from PIL import ImageTk, Image
+from PIL import Image
 import os
 
 #-----archivos-------
@@ -34,6 +33,12 @@ class Inicio:
             respuesta = tk.messagebox.askquestion("Saliendo de UNON'T", "¿Estas seguro de salir?")
             if respuesta == "yes" :
                 salir()
+        
+        def ir_a_prejuego():
+            # esta funcion cerrara el menu principal para posteriormente abrir la prejuego
+            salir()
+            Ventana_pre_juego = opciones_pre_juego()
+
 
         #------------------------Controles de self---------------
 
@@ -45,7 +50,53 @@ class Inicio:
 
         etiqueta.pack(pady=15)
 
-        ctk.CTkButton(self.root, text="Iniciar partida").pack(pady=10)
+        ctk.CTkButton(self.root, text="Iniciar partida", command=ir_a_prejuego).pack(pady=10)
+        ctk.CTkButton(self.root, text="Opciones").pack(pady=10)
 
 
         self.root.mainloop()
+#-----funciones extras------
+class funciones:
+    
+    def ventana_jugadores_2(self):
+        ventana = ctk.CTkToplevel()
+        ventana.title("Ventana de jugadores")
+
+    def ventana_jugadores_3(self):
+        ventana = ctk.CTkToplevel()
+        ventana.title("Ventana de jugadores")
+
+    def ventana_jugadores_4(self):
+        ventana = ctk.CTkToplevel()
+        ventana.title("Ventana de jugadores")
+
+    def salir(self):
+        ventana = ctk.CTkToplevel()
+        ventana.destroy()
+
+
+    def backtoMainMenu(self):
+        ventana = ctk.CTkToplevel()
+        ventana.destroy()
+        ventana_back = Inicio()
+
+objeto_func = funciones()
+
+#-------nueva ventana-------
+class opciones_pre_juego:
+    botones = {'2 jugadores': objeto_func.ventana_jugadores_2, '3 jugadores': objeto_func.ventana_jugadores_3, '4 jugadores': objeto_func.ventana_jugadores_4, 'volver al menu': objeto_func.backtoMainMenu,'salir del juego': objeto_func.salir}
+    def __init__(self):
+        #---------------------------self-------------------------
+        self.root = ctk.CTk()
+        self.root.title("Sala de jugadores.")
+
+        cont = 0
+
+        for texto_boton in self.botones:
+            button = ctk.CTkButton(master=self.root, text= texto_boton, height=50, width=200, command= self.botones[texto_boton])
+
+            button.grid(row=cont//2, column=cont%2, padx=5, pady=5)   
+            cont += 1
+            
+
+        self.root.mainloop() 
